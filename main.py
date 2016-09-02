5bb1400938d4157f6ac0d374e993fc94d1438cf8
Adam <adamlang@kth.se>
2016-09-02 21:54:19 +0200

    Assignment 1 ready

--- main.py
+++ main.py
@@ -2,11 +2,20 @@ import monkdata as m
 import math
 import dtree
 
-monk1_entropy = dtree.entropy(m.monk1)
-monk2_entropy = dtree.entropy(m.monk2)
-monk3_entropy = dtree.entropy(m.monk3)
 
-print(monk1_entropy)
-print(monk2_entropy)
-print(monk3_entropy)
+# Assignment 1 #
+################
 
+monk_entropy = []
+data_sets = [m.monk1, m.monk2, m.monk3]
+
+i = 0
+for set in data_sets:
+    monk_entropy.append(dtree.entropy(set))
+    i += 1
+
+for i in range(3):
+    print(monk_entropy[i])
+
+# Assignment 2 #
+################
